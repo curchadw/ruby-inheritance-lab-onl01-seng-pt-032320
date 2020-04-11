84782a1db790abf2db6eabef58b0c23463cbbedc
Curtis Chadwell <laurencechadwell@gmail.com>
2020-04-11 18:09:22 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,3 +1,13 @@
 class User
 
+attr_accessor :first_name, :last_name
+
+def initialize(first_name,last_name)
+  @first_name = first_name
+  @last_name = last_name
+  @knowledge = Array.new
+end
+
+
+
 end
